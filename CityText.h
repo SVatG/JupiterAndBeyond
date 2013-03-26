bedd40b0b23d64a70a8d1778b5cdcc0d26d596d2
halcy <lorenzd@gmail.com>
2013-03-27 00:03:59 +0100

    Added things.

---
+++ CityText.h
@@ -0,0 +1,9 @@
+#ifndef __CITY_TEXT_H__
+#define __CITY_TEXT_H__
+
+extern const Pixel citytext1_pixels[NumberOfPixelsForWidthAndHeight(16,16)];
+extern const Pixel citytext2_pixels[NumberOfPixelsForWidthAndHeight(16,16)];
+extern const Pixel citytext3_pixels[NumberOfPixelsForWidthAndHeight(16,16)];
+extern const Pixel citytext4_pixels[NumberOfPixelsForWidthAndHeight(16,16)];
+
+#endif
